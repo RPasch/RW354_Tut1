c01b0a0184c235ec5a3fcdc8a51163b03ddb1c72
Rabbo <18214304@sun.ac.za>
2018-07-27 13:22:49 +0200

    server folder

---
+++ rw354_tut1_server/src/rw354_tut1_server/Rw354_tut1_server.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rw354_tut1_server;
+
+/**
+ *
+ * @author 18214304
+ */
+public class Rw354_tut1_server {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
