f4bd98579682d08c031d56721698094435335286
Rabbo <18214304@sun.ac.za>
2018-07-27 13:21:15 +0200

    project folders

---
+++ RW354_tut1_client/src/rw354_tut1_client/RW354_tut1_client.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rw354_tut1_client;
+
+/**
+ *
+ * @author 18214304
+ */
+public class RW354_tut1_client {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
