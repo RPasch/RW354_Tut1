08f4fbdebb944fb5974968fc5b3e2f548d495250
Joshua Coetzer <20059884@sun.ac.za>
2018-07-29 15:59:57 +0200

    work on server

--- rw354_tut1_server/src/rw354_tut1_server/Server.java
+++ rw354_tut1_server/src/rw354_tut1_server/Server.java
@@ -1,18 +1,36 @@
 package rw354_tut1_server;
 
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
+    // use concurrent hashmap
     public static void main(String[] args) {
         int portNumber = Integer.parseInt(args[0]);
-        
-        //System.out.println(portNumber);
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;        
         
         try {
-            ServerSocket serverSocket = new ServerSocket(portNumber);
+            serverSocket = new ServerSocket(portNumber);
             System.out.println(serverSocket);
+            //create socket handler class
+            //constructor takes client socket
+            //
         } catch (Exception e) {
             System.exit(0);
+        }
+        
+        while(true){
+            try {
+                clientSocket = serverSocket.accept();
+                SocketHandler sh = new SocketHandler(clientSocket);
+                new Thread(sh).start();
+                
+                System.out.println("new user connected");
+                 
+           } catch (Exception e) {
+               
+           }
         }
         
     }
